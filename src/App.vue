604630dad4b8951480aea82f30bf8473da15aa19
SumiyaMim <sumiyaislam1093@gmail.com>
2025-05-12 22:33:42 +0600

    add projects section

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,7 @@
         <Services/>
         <Skills/>
         <Whyme/>
+        <Projects/>
     </div>
 </template>
 
@@ -19,4 +20,5 @@
     const Services = defineAsyncComponent(() => import('@/components/layout/Services.vue'));
     const Skills = defineAsyncComponent(() => import('@/components/layout/Skills.vue'));
     const Whyme = defineAsyncComponent(() => import('@/components/layout/Whyme.vue'));
+    const Projects = defineAsyncComponent(() => import('@/components/layout/Projects.vue'));
 </script>
